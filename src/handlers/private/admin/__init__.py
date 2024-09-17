36d2613a61bc22517bf8bf8b9a3381d0455376f1
umarr <umarr.developer@gmail.com>
2024-09-17 12:35:29 +0600

    set new admin handler

--- src/handlers/private/admin/__init__.py
+++ src/handlers/private/admin/__init__.py
@@ -2,9 +2,11 @@ from aiogram import Router
 
 from src.filters.chat import AdminFilter
 from src.handlers.private.admin.info import router as info_router
+from src.handlers.private.admin.prompt import router as prompt_router
 
 router = Router()
 
 router.include_router(info_router)
+router.include_router(prompt_router)
 router.message.filter(AdminFilter())
 router.callback_query.filter(AdminFilter())
