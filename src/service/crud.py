3105ad7c0d9f962197a34baff842ddceecc92134
umarr <umarr.developer@gmail.com>
2024-09-17 12:33:28 +0600

    add crud commands

--- src/service/crud.py
+++ src/service/crud.py
@@ -1,8 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.baked import Result
 
-from src.models import User
+from src.models import User, Prompt
 
 
 async def create_user(sessions: async_sessionmaker, user_id: int) -> User:
@@ -32,3 +32,31 @@ async def get_all_users(sessions: async_sessionmaker) -> list[User]:
     async with sessions() as session:
         result: Result = await session.execute(sql)
     return result.scalars().all()
+
+
+async def create_promt(sessions: async_sessionmaker,
+                       content: str,
+                       prompt_id: int = 1) -> Prompt:
+    prompt = Prompt(promt_id=1, content=content)
+    async with sessions() as session:
+        session.add(prompt)
+        await session.commit()
+    return prompt
+
+
+async def update_prompt(sessions: async_sessionmaker,
+                        content: str,
+                        prompt_id: int = 1):
+    sql = update(Prompt).where(Prompt.promt_id == prompt_id).values(content=content)
+    async with sessions() as session:
+        await session.execute(sql)
+        await session.commit()
+
+
+async def get_prompt(sessions: async_sessionmaker,
+                     prompt_id: int = 1) -> Prompt:
+    sql = select(Prompt).where(Prompt.promt_id == prompt_id)
+    async with sessions() as session:
+        result: Result = await session.execute(sql)
+        await session.commit()
+    return result.scalar()
