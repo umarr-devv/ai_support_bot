b1ad1b28bc1bf7cc77aac8e25b3f1c6b5d802e98
umarr <umarr.developer@gmail.com>
2024-09-20 21:28:20 +0600

    add 'about' handlers

---
+++ src/handlers/private/user/about.py
@@ -0,0 +1,16 @@
+from aiogram import types, Router, F
+from aiogram.filters import CommandStart, CommandObject
+
+router = Router()
+
+
+@router.message(F.text == '📒 О компании')
+async def on_about_company(message: types.Message):
+    text = 'Информация о компании'
+    await message.answer(text=text)
+
+
+@router.message(F.text == '❕ О боте')
+async def on_about_company(message: types.Message):
+    text = 'Информация о боте'
+    await message.answer(text=text)
