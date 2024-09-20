bb8161597bdeb509f3cd939cb26cdb312049c56e
umarr <umarr.developer@gmail.com>
2024-09-20 21:28:43 +0600

    include 'about' handlers to user router

--- src/handlers/private/user/__init__.py
+++ src/handlers/private/user/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Router
 
+from src.handlers.private.user.about import router as about_router
 from src.handlers.private.user.start import router as start_router
 
 router = Router()
 
 router.include_router(start_router)
+router.include_router(about_router)
